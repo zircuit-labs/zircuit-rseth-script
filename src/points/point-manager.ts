d950c83a071001e173fcd886bd25a5b45f654fc6
F/LALIDJI <flalidji@protonmail.com>
2024-06-21 11:03:02 +0400

    Update src/points/point-manager.ts

--- src/points/point-manager.ts
+++ src/points/point-manager.ts
@@ -40,7 +40,7 @@ export function updatePoints(
   const zPoint = calcPointsFromHolding(
     amountRsEthHolding,
     holdingStartTimestamp,
-    holdingEndTimestamp,
+    holdingEndTimestamp
   );
 
   if (label == POINT_SOURCE_YT) {
